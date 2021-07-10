ddaeaaa5dfcfbfd98f151b5b90e7f75092c168eb
Johannes Christ <jc@jchri.st>
2021-07-10 15:29:30 +0200

    Rename `v1` package to `endpoints`.

---
+++ api/endpoints/__init__.py
@@ -0,0 +1,7 @@
+"""
+Endpoint definitions of the Python Discord API.
+
+This package contains all the route definitions of the Python Discord API.
+There are currently no plan to use a strictly versioned API design, as this API
+is currently tightly coupled with a single client application.
+"""
