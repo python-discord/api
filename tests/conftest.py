7a7f02096905e9a4fc5a6aca0a03ef6034364242
D0rs4n <41237606+D0rs4n@users.noreply.github.com>
2021-11-29 21:53:50 +0100

    Increase code consistency in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,6 +19,7 @@ test_engine = create_async_engine(settings.database_url, future=True, isolation_
 
 @pytest.fixture(scope="session")
 def event_loop(request) -> Generator:
+    """Yields back an asyncio event loop, then closes it."""
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
@@ -26,6 +27,11 @@ def event_loop(request) -> Generator:
 
 @pytest.fixture(scope="session")
 async def create_test_database_engine(worker_id) -> Generator:
+    """
+    Yields back a Database engine object
+
+    Thix fixture will automatically create and delete the databases based on the current worker's id.
+    """
     async with test_engine.begin() as conn:
         await conn.execute(text(f"CREATE DATABASE {worker_id}_test;"))
         test_db_url = urlsplit(settings.database_url)._replace(path=f"/{worker_id}_test")
@@ -37,6 +43,12 @@ async def create_test_database_engine(worker_id) -> Generator:
 
 @pytest.fixture()
 async def async_db_session(create_test_database_engine: AsyncEngine) -> AsyncSession:
+    """
+    Yields back an Asynchronous database session
+
+    This fixture requests `create_test_database_engine` fixture, as its dependency, this way the session
+    will point to the database created using the current worker's id.
+    """
     async with create_test_database_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
@@ -45,15 +57,9 @@ async def async_db_session(create_test_database_engine: AsyncEngine) -> AsyncSes
             await session.close()
 
 
-@pytest.fixture(scope="session", autouse=True)
-async def global_teardown(create_test_database_engine: AsyncEngine):
-    yield
-    async with create_test_database_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-
-
 @pytest.fixture()
 def override_db_session(async_db_session: AsyncSession):
+    """Yields back the modified Database session that uses the correspondent Database"""
     async def _override_db_session():
         yield async_db_session
 
@@ -62,17 +68,20 @@ def override_db_session(async_db_session: AsyncSession):
 
 @pytest.fixture()
 def app(override_db_session: Callable):
+    """Yields back a patched version of the main app so that it uses the correspondent DB. session"""
     main_app.dependency_overrides[create_database_session] = override_db_session
     yield main_app
 
 
 @pytest.fixture()
 async def unauthed_client(app: FastAPI):
+    """Yields back an unauthenticated HTTP Client"""
     async with AsyncClient(app=app, base_url="http://testserver") as httpx_client:
         yield httpx_client
 
 
 @pytest.fixture()
 async def client(app: FastAPI):
+    """Yield back an authenticated HTTP Client"""
     async with AsyncClient(app=app, base_url="http://testserver", headers=AUTH_HEADER) as httpx_client:
         yield httpx_client
